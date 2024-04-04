080d38fd77320ac7191b46c16e6339dea48aa898
kevinkennedy <@kennedy.kev@northeastern.edu>
2024-04-04 03:24:25 -0400

    tres

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
-import CourseRoutes from './kanbas-node-server-app/Kanbas/courses/routes.js';
-import ModuleRoutes from "./kanbas-node-server-app/Kanbas/modules/routes.js";
+import CourseRoutes from './Kanbas/courses/routes.js';
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
 const app = express()
 
