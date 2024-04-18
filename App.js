dea1b7ae87a2f0f3f14f24fa07f661453d1d0edd
kevinkennedy <@kennedy.kev@northeastern.edu>
2024-04-18 03:42:54 -0400

    123

--- App.js
+++ App.js
@@ -11,8 +11,9 @@ import "dotenv/config";
 
 
 const app = express()
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/kanbas';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
+ 
 
 const sessionOptions = {
     secret: process.env.SESSION_SECRET,
